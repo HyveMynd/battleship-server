79d8e2bb593796110fef4b62046ccf2714b925f8
Andres Monroy <andresf.mon@gmail.com>
2014-11-05 23:14:53 -0700

    fixed regex validation

--- routes/games.js
+++ routes/games.js
@@ -155,11 +155,11 @@ var createPlayer = function (name) {
 };
 
 var isAlpha = function (string) {
-    return string && string.match('[A-Za-z0-9]');
+    return string && string.match(/^[a-z0-9]+$/i);
 };
 
 var isGUID = function (string) {
-    return string.match('^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$');
+    return string.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
 };
 
 var checkValidGame = function (req, res, next) {
