e5728483096dca81889c47d2a7865ebbcbb00de7
Andres Monroy <andresf.mon@gmail.com>
2014-11-05 23:40:23 -0700

    added production error handling

--- index.js
+++ index.js
@@ -26,5 +26,35 @@ app.use(express.static(path.join(__dirname, './public')));
 require('./routes/games')(app, express);
 require('./routes/home')(app, express);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        console.log(err);
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 app.listen(3000);
 console.log('Magic happens on port 3000');
