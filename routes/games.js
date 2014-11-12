90009861be298679e11c869ad7894aed603b09fe
Andres Monroy <andresf.mon@gmail.com>
2014-11-11 18:52:46 -0700

    fixed bug where isGUID was not checking for undefined

--- routes/games.js
+++ routes/games.js
@@ -161,7 +161,7 @@ var isAlpha = function (string) {
 };
 
 var isGUID = function (string) {
-    return string.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
+    return string && string.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
 };
 
 var checkValidGame = function (req, res, next) {
